494d0bfc5035e62c6847fba6f51d6e6399e2d424
Marco Morelos <mmore019@sledge.cs.ucr.edu>
2015-05-20 08:51:15 -0700

    removed some of the old code

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -18,13 +18,6 @@ syscall_init (void)
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
-//static void
-//syscall_handler (struct intr_frame *f UNUSED) 
-//{
-  //printf ("system call!\n");
-  //thread_exit ();
-//}
-
 static void
 syscall_handler (struct intr_frame *f)
 {
